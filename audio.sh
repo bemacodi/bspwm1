13ff55525696ad7876794a296aaf6c439840c940
Benjamín M <10850897+bemacodi@users.noreply.github.com>
2022-12-19 23:19:53 -0600

    Add files via upload

---
+++ audio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -S alsa-firmware-loaders alsa-oss alsa-tools alsa-utils alsamixergui volumeicon-alsa paman paprefs pavumeter pulseaudio-utils pulseaudio-esound-compat ffmpeg2theora sound-icons
+
+sudo pacman -S lame libdvdnav4 libdvdread4 libfaac0 libmad0 libmp3lame0 libquicktime2 libstdc++5 libxvidcore4 twolame vorbis-tools x264
+
+sudo pacman -S  gstreamer1.0-x gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools
+
+sudo pacman -S gst-libav
